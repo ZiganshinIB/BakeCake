bf88d3059d244ba4c9d023aa261fa3526f0cc8c9
ZiganshinIB <elzig3012@gmail.com>
2024-08-20 22:15:17 +0300

    Add urls

--- cake/api_urls.py
+++ cake/api_urls.py
@@ -1,12 +1,18 @@
 from django.urls import include, path
 
-from .api_views import CakeViewSet, CakeLevelViewSet
+from .api_views import CakeViewSet, CakeLevelViewSet, CakeShapeViewSet, CakeToppingViewSet, CakeBerryViewSet, \
+    CakeDecorViewSet, OrderViewSet
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 router = SimpleRouter()
 router.register(r'cakes', CakeViewSet)
 router.register(r'cakelevels', CakeLevelViewSet)
+router.register(r'cakeshape', CakeShapeViewSet)
+router.register(r'caketopping', CakeToppingViewSet)
+router.register(r'cakeberry', CakeBerryViewSet)
+router.register(r'cakedecor', CakeDecorViewSet)
 
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
