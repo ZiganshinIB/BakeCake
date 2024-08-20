67dffc1c4cee03897399318003df2744b63ee2dd
ZiganshinIB <elzig3012@gmail.com>
2024-08-20 22:00:13 +0300

    Refract: move file

--- cake/views.py
+++ cake/views.py
@@ -10,7 +10,7 @@ from .models import Cake
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'cake/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
