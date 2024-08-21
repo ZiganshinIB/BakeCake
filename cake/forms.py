fc620159d433e420a34b0ab2dcc870e5d2bbb2ab
ZiganshinIB <elzig3012@gmail.com>
2024-08-21 22:46:41 +0300

    Формы для ввода телефона и пин кода

---
+++ cake/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+
+class PhoneForm(forms.Form):
+    phone_number = forms.CharField(
+        max_length=15,
+        widget=forms.TextInput(attrs={
+            'class': 'contacts__form_iunput',
+            'placeholder': '+7(999)999--99-99',
+            'required': 'required'
+        })
+    )
+
+
+class PinForm(forms.Form):
+    pin = forms.CharField(
+        max_length=4,
+        widget=forms.TextInput(attrs={
+            'class': 'tipsPopup__form_inputNum popup__input',
+            'placeholder': '9999',
+            'required': 'required',
+            'style': 'width: 200px; height: 50px;'
+        })
+    )
