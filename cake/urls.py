36c5e4dcf45f002a4a6820020c9c44d327acbda2
ZiganshinIB <elzig3012@gmail.com>
2024-08-21 22:49:14 +0300

    Добавлен маршруты для регистрации

--- cake/urls.py
+++ cake/urls.py
@@ -1,6 +1,9 @@
+from multiprocessing.resource_tracker import register
+
 from .views import *
 from django.urls import path
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('register/', registration, name='registration'),
 ]
