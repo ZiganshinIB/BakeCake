30ba40135333937c99b4f51cb18f2cf2f6105cd0
ZiganshinIB <elzig3012@gmail.com>
2024-08-26 19:45:43 +0300

    Refract

--- cake/urls.py
+++ cake/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('register/', registration, name='registration'),
     path('lk/', ProfileView.as_view(), name='lk'),
     path('catalog/', CakeCatalogView.as_view(), name='catalog'),
-    path('cakes/', CakesListView.as_view(), name='cakes'),
 ]
